5d7ccf0084562ddff18d646732349377900fc01a
Johann Malm <cjcmalm@icloud.com>
2019-06-05 19:47:43 +0200

    footer and header

---
+++ library/FooterView.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * FooterView
+ */
+class FooterView extends View
+{
+  private $id = "";
+
+  public function __construct($id)
+  {
+    $this->id = $id;
+  }
+
+  public static function setId($id)
+  {
+    return new self($id);
+  }
+
+  public function __toString()
+  {
+    $html = self::setTag('div')
+    ->setAttribute('class', 'footer')
+    ->setAttribute('id', $this->id);
+
+    foreach ($this->getAttribute() as $key => $value) {
+      foreach ($value as $attribute) {
+        $html->setAttribute($key, $attribute);
+      }
+    }
+
+    foreach ($this->getContent() as $content) {
+      $html->setContent($content);
+    }
+
+    return (string) $html;
+  }
+}
+
+
+?>
---
+++ library/HeaderView.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * HeaderView
+ */
+class HeaderView extends View
+{
+  private $id = "";
+
+  public function __construct($id)
+  {
+    $this->id = $id;
+  }
+
+  public static function setId($id)
+  {
+    return new self($id);
+  }
+
+  public function __toString()
+  {
+    $html = self::setTag('div')
+    ->setAttribute('class', 'header')
+    ->setAttribute('id', $this->id);
+
+    foreach ($this->getAttribute() as $key => $value) {
+      foreach ($value as $attribute) {
+        $html->setAttribute($key, $attribute);
+      }
+    }
+
+    foreach ($this->getContent() as $content) {
+      $html->setContent($content);
+    }
+
+    return (string) $html;
+  }
+}
+
+
+?>
